c2c7482b309ec9eac2057eda992c352de20db8d4
Yifan Xu <xuyf@verona.cs.washington.edu>
2019-06-04 11:22:44 -0700

    ll

--- mainprogram.py
+++ mainprogram.py
@@ -1,12 +1,13 @@
 import sys
 import json
+import math
 from os import path
 
 import cv2
 import numpy as np
 
 
-from PyQt5.QtGui import QPixmap, QCursor, QPainter, QImage
+from PyQt5.QtGui import QPixmap, QCursor, QPainter, QImage, QColor, QBrush
 from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QApplication, QMainWindow
 from PyQt5 import QtCore, QtWidgets, QtGui
 
@@ -24,6 +25,7 @@ class VideoReader:
     def get_frame(self, index):
         self.cap.set(cv2.CAP_PROP_POS_FRAMES, index-1)
         res, frame = self.cap.read()
+        print("get frame")
         return frame
 
 class VideoWriter:
@@ -36,6 +38,8 @@ class DrawFace:
         self.font = cv2.FONT_HERSHEY_SIMPLEX
 
     def draw_faces(self, frame, index):
+        while self.data.get(str(index)) is None and index >= 0:
+            index -= 1
         faces = self.data[str(index)]
         for idx, box in faces.items(): # for box in faces:
             L, T, R, B = box['box']
@@ -71,23 +75,18 @@ class DrawFace:
 '''
 class RecordVideo(QtCore.QObject):
         image_data = QtCore.pyqtSignal(np.ndarray)
-
         def __init__(self, camera_port=0, parent=None):
                 super().__init__(parent)
                 self.camera = cv2.VideoCapture(camera_port)
                 self.timer = QtCore.QBasicTimer()
-
         def start_recording(self):
                 self.timer.start(0, self)
-
         def timerEvent(self, event):
                 if (event.timerId() != self.timer.timerId()):
                         return
-
                 read, image = self.camera.read()
                 if read:
                         self.image_ready.emit(image)
-
 class FaceDetectionWidget(QtWidgets.QWidget):
         def __init__(self, haar_cascade_filepath, parent=None):
                 super().__init__(parent)
@@ -96,70 +95,55 @@ class FaceDetectionWidget(QtWidgets.QWidget):
                 self._red = (0, 0, 255)
                 self._width = 2
                 self._min_size = (30, 30)
-
         def detect_faces(self, image: np.ndarray):
                 # haarclassifiers work better in black and white
                 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
                 gray_image = cv2.equalizeHist(gray_image)
-
                 faces = self.classifier.detectMultiScale(gray_image, scaleFactor=1.3, minNeighbors=4, flags=cv2.CASCADE_SCALE_IMAGE, minSize=self._min_size)
-
                 return faces
-
         def image_data_slot(self, image_data):
                 faces = self.detect_faces(image_data)
                 for (x, y, w, h) in faces:
                         cv2.rectangle(image_data, (x, y), (x+w, y+h), self._red, self._width)
-
                 self.image = self.get_qimage(image_data)
                 if self.image.size() != self.size():
                         self.setFixedSize(self.image.size())
-
                 self.update()
-
         def get_qimage(self, image: np.ndarray):
                 height, width, colors = image.shape
                 bytesPerLine = 3 * width
                 QImage = QtGui.QImage
-
                 image = QImage(image.data, width, height, bytesPerLine, QImage.Format_RGB888)
-
                 image = image.rgbSwapped()
                 return image
-
         def paintEvent(self, event):
                 painter = QtGui.QPainter(self)
                 painter.drawImage(0, 0, self.image)
                 self.image = QtGui.QImage()
-
 class MainWidget(QtWidgets.QWidget):
         def __init__(self, haarcascade_filepath, parent=None):
                 super().__init__(parent)
                 fp = haarcascade_filepath
                 self.face_detection_widget = FaceDetectionWidget(fp)
-
                 # TODO: set video port
                 self.record_video = RecordVideo()
                 self.run_button = QtWidgets.QPushButton('Start')
-
                 # Connect the image data signal and slot together
                 image_data_slot = self.face_detection_widget.image_data_slot
                 self.record_video.image_data.connect(image_data_slot)
                 # connect the run button to the start recording slot
                 self.run_button.clicked.connect(self.record_video.start_recording)
-
                 # Create and set the layout
                 layout = QtWidgets.QVBoxLayout()
                 layout.addWidget(self.face_detection_widget)
                 layout.addWidget(self.run_button)
-
                 self.setLayout(layout)
 '''
 
-class Example(QWidget):        
+class FrameWidget(QWidget):        
     def __init__(self):
-        super(Example, self).__init__()    
-
+        super(FrameWidget, self).__init__()    
+        self.image = QtGui.QImage("001.png")
         self.initUI()
 
     def mousePressEvent(self, QMouseEvent):
@@ -175,80 +159,136 @@ class Example(QWidget):
         #qbtn = QPushButton('Quit', self)
         #qbtn.resize(qbtn.sizeHint())
         #qbtn.move(50, 50)       
-        reader = VideoReader(file_path)
-        painter = DrawFace(data_path)
-        self.setGeometry(0, 0, 1024, 768)
-        self.setWindowTitle('Quit button')    
+        self.reader = VideoReader(file_path)
+        self.faces = DrawFace(data_path)
+
         self.setWindowFlags(self.windowFlags() | QtCore.Qt.FramelessWindowHint)
 
         #self.statusBar().showMessage('This is a status bar') 
 
-        label = QLabel(self)
+        #label = QLabel(self)
 
-
-        frame_num = 0
-        frame = reader.get_frame(frame_num)
-        painter.draw_faces(frame, frame_num)
-
-        image = self.get_qimage(frame)
-
-        pixmap = QPixmap(QPixmap.fromImage(image))
-        
         '''
         # create painter instance with pixmap
         self.painterInstance = QPainter(pixmap)
-
         # set rectangle color and thickness
         self.penRectangle = QtGui.QPen(QtCore.Qt.red)
         self.penRectangle.setWidth(3)
-
         # draw rectangle on painter
         self.painterInstance.setPen(self.penRectangle)
         self.painterInstance.drawRect(590,150,42,55)
-
         # set pixmap onto the label widget
         #self.ui.label_imageDisplay.setPixmap(pixmap)
         #self.ui.label_imageDisplay.show()
         '''
-        label.setPixmap(pixmap)
-        self.resize(pixmap.width(),pixmap.height())
-        label.move(0, 0)
-        self.show()
+        #label.setPixmap(pixmap)
+        #self.resize(pixmap.width(),pixmap.height())
+        #label.move(0, 0)
+        #self.show()
 
-    def image_data_slot(self, image_data):
-            faces = self.detect_faces(image_data)
-            for (x, y, w, h) in faces:
-                    cv2.rectangle(image_data, (x, y), (x+w, y+h), self._red, self._width)
+    def image_data_slot(self, frame_num):
+        frame = self.reader.get_frame(frame_num)
+        self.faces.draw_faces(frame, frame_num)
 
-            self.image = self.get_qimage(image_data)
-            if self.image.size() != self.size():
-                    self.setFixedSize(self.image.size())
+        self.image = self.get_qimage(frame)
 
-            self.update()
+        #pixmap = QPixmap(QPixmap.fromImage(image))
+        if self.image.size() != self.size():
+                self.setFixedSize(self.image.size())
+
+        self.update()
 
     def get_qimage(self, image: np.ndarray):
-            height, width, colors = image.shape
-            bytesPerLine = 3 * width
-            QImage = QtGui.QImage
+        height, width, colors = image.shape
+        bytesPerLine = 3 * width
+        QImage = QtGui.QImage
 
-            image = QImage(image.data, width, height, bytesPerLine, QImage.Format_RGB888)
+        image = QImage(image.data, width, height, bytesPerLine, QImage.Format_RGB888)
 
-            image = image.rgbSwapped()
-            return image
+        image = image.rgbSwapped()
+        return image
 
-    def test(self):
-      print("test")
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.drawImage(0, 0, self.image)
+        # self.image = QtGui.QImage()
+
+class BarWidget(QWidget):
+    
+    def __init__(self, frame_widget, frame_num):
+        super().__init__()
+        self.frame_widget = frame_widget
+        self.frame_num = frame_num
+    #     self.initUI()
+        
+        
+    # def initUI(self):      
+
+    #     self.setGeometry(300, 300, 350, 100)
+    #     self.setWindowTitle('Colours')
+    #     self.show()
+
+    def mousePressEvent(self, QMouseEvent):
+        index = math.ceil(QMouseEvent.x() * self.frame_num / self.width())
+        print(str(index))
+        self.frame_widget.image_data_slot(index)
+
+    def mouseReleaseEvent(self, QMouseEvent):
+        cursor =QCursor()
+        #self.statusBar().showMessage('(' + str(QMouseEvent.x()) + ', '+  str(QMouseEvent.y()) + ')')
+        print('(', QMouseEvent.x(), ', ', QMouseEvent.y(), ')')
+        # self.frame_widget.image_data_slot(29)
+        print(cursor.pos())  
+
+    def paintEvent(self, e):
+
+        qp = QPainter()
+        qp.begin(self)
+        self.drawRectangles(qp)
+        qp.end()
+
+        
+    def drawRectangles(self, qp):
+      
+        col = QColor(0, 0, 0)
+        col.setNamedColor('#d4d4d4')
+        qp.setPen(col)
+
+        qp.setBrush(QColor(255, 255, 255))
+        qp.drawRect(0, 0, self.width(), 60)
+
+        # qp.setBrush(QColor(25, 0, 90, 200))
+        # qp.drawRect(250, 15, 90, 60)
+
+class MainWidget(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.frame_widget = FrameWidget()
+        self.setGeometry(0, 0, 1030, 768)
+        self.setWindowTitle('Quit button')    
+        self.run_button = QtWidgets.QPushButton('Start')
+        self.bar = BarWidget(self.frame_widget, self.frame_widget.reader.frame_num)
+
+        # Connect the image data signal and slot together
+        image_data_slot = self.frame_widget.image_data_slot(0)
+
+        # Create and set the layout
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.frame_widget)
+        layout.addWidget(self.run_button)
+        layout.addWidget(self.bar)
+
+        self.setLayout(layout)
 
 def main():        
         app = QApplication(sys.argv)
         main_window = QtWidgets.QMainWindow()
-        main_widget = Example()
+        main_widget = MainWidget()
         main_window.setCentralWidget(main_widget)
+        main_window.resize(1024, 768)
         main_window.show()
         sys.exit(app.exec_())
 
 if __name__ == '__main__':
          main()
-
-
-
